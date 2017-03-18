c948f7eec8400783489343532b716dce2cd83277
Ubuntu <qa@question-answerer2.eagvnp5ywm3ufb3k4jgdkefvcb.ex.internal.cloudapp.net>
2017-03-18 06:33:11 +0000

    ran more things, fixed up the cluster file

--- code/cluster.py
+++ code/cluster.py
@@ -1,13 +1,13 @@
 from sklearn.cluster import KMeans
 import numpy as np
 
-X = np.load('/mnt/encoded_files/encoded0.npz')['data']
-for i in xrange(1,3):
+X = np.load('/mnt/final_encoded_files/encoded0.npz')['data']
+for i in xrange(1,16):
 	print i
-	filen = '/mnt/encoded_files/encoded' + str(i) + '.npz'
+	filen = '/mnt/final_encoded_files/encoded' + str(i) + '.npz'
 	X = np.concatenate((X,np.load(filen)['data']),axis=0)
 
 kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 
-print kmeans.labels_
-print kmeans.cluster_centers_
+np.savez('data/kmeans_labels',data=kmeans.labels_)
+np.savez('data/kmeans_clusters',data=kmeans.cluster_centers_)
