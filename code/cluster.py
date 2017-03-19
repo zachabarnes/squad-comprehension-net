db08311b5be7edd66b7c35635bde2cb08fe59961
Frank Cipollone <frank@DN51o0c8.SUNet>
2017-03-18 18:53:11 -0700

    changed cluster.py

--- code/cluster.py
+++ code/cluster.py
@@ -1,13 +1,23 @@
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 import numpy as np
 
-X = np.load('/mnt/encoded_files/encoded0.npz')['data']
-for i in xrange(1,3):
+X = np.load('/mnt/final_encoded_files/encoded0.npz')['data']
+for i in xrange(1,16):
 	print i
-	filen = '/mnt/encoded_files/encoded' + str(i) + '.npz'
+	filen = '/mnt/final_encoded_files/encoded' + str(i) + '.npz'
 	X = np.concatenate((X,np.load(filen)['data']),axis=0)
 
-kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
+#kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 
-print kmeans.labels_
-print kmeans.cluster_centers_
+pca = PCA(n_components = 2).fit(X)
+
+t = pca.transform(X)
+
+np.savez('pcad',data=t)
+
+#plt.plot(t[:,0],t[:,1])
+#plt.show()
+#np.savez('data/kmeans_labels',data=kmeans.labels_)
+#np.savez('data/kmeans_clusters',data=kmeans.cluster_centers_)
